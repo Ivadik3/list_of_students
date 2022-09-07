8bf607a7955c9431d47bdc58768d4b5a4b8200d3
Ivadik3 <sava12300@gmail.com>
2022-09-07 09:41:47 +0300

    some commit idk

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 import imp
-from flask import Flask
+from flask import Flask,render_template
+from Data_Manager import Data_Manager
 
+#данный класс используется для задания всех возможных страниц сайта
+#и методы их формирования
 #основной класс приложения, где создаем объект фласки
 
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 
 @app.route("/main")
 def main_page():
+    list_of_posts=[]
     with open("static\data\students.json","a") as file:
+        D_manager=Data_Manager()
+        list_of_posts = D_manager.get_all_posts()
         
-        
-    return render_template("students.html")
+    return render_template("students.html",list_of_posts)
 
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
->>>>>>> origin/main
