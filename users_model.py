01edc1d37f5086cc27ed9cd4c0a6516076dc2ff0
Ivadik3 <sava12300@gmail.com>
2022-09-13 16:25:44 +0300

    feel better today, got concept of POST algorhytm

---
+++ users_model.py
@@ -0,0 +1,27 @@
+
+
+
+
+class User():
+    NOT_DEFINED = "not defined"
+    def __init__(self,id,name,gender):
+        self.ID = id
+        self.Name = name
+        self.Gender = gender
+        self.class_ = User.NOT_DEFINED
+        self.Seat = User.NOT_DEFINED
+        self.Club = User.NOT_DEFINED
+        self.Persona = User.NOT_DEFINED
+        self.Crush = User.NOT_DEFINED
+        self.BreastSize = User.NOT_DEFINED
+        self.Strength = User.NOT_DEFINED
+        self.Hairstyle = User.NOT_DEFINED
+        self.Color = User.NOT_DEFINED
+        self.Eyes = User.NOT_DEFINED
+        self.EyeType = User.NOT_DEFINED
+        self.Stockings = User.NOT_DEFINED
+        self.Accessory = User.NOT_DEFINED
+        self.ScheduleTime = User.NOT_DEFINED
+        self.ScheduleDestination = User.NOT_DEFINED
+        self.ScheduleAction = User.NOT_DEFINED
+        self.Info = User.NOT_DEFINED 
